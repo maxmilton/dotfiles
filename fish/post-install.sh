89de8280a1118e18ad17d74576faf9f3a2d9166b
Max Milton <max@wearegenki.com>
2018-04-08 08:54:03 +1000

    Uncomment code (leftover from debugging)

--- fish/post-install.sh
+++ fish/post-install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# # check if Fisherman is installed
-# if [[ ! -f "$TARGET_DIR/.config/fish/functions/fisher.fish" ]]; then
-#   echo_warn "Fisherman is not installed, installing it now..."
+# check if Fisherman is installed
+if [[ ! -f "$TARGET_DIR/.config/fish/functions/fisher.fish" ]]; then
+  echo_warn "Fisherman is not installed, installing it now..."
 
-#   # install Fisher
-#   curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-# fi
+  # install Fisher
+  curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+fi
 
-# # check fish shell is installed
-# if hash fish 2>/dev/null; then
-#   # install Fisherman plugins (always exit 0 to prevent stopping package install)
-#   echo_info "Installing Fisher theme and plugins."
-#   [[ $dryrun != true ]] && fish -c 'fisher rafaelrinaldi/pure fzf z' || true
+# check fish shell is installed
+if hash fish 2>/dev/null; then
+  # install Fisherman plugins (always exit 0 to prevent stopping package install)
+  echo_info "Installing Fisher theme and plugins."
+  [[ $dryrun != true ]] && fish -c 'fisher rafaelrinaldi/pure fzf z' || true
 
-#   # generate a compiled fish user config
-#   echo_info "Setting up fish user config."
-#   [[ $dryrun != true ]] && fish "$TARGET_DIR/.config/fish/run-once.fish"
-# else
-#   echo_err "Fish shell is required to install Fisher plugins. Skipping."
-# fi
+  # generate a compiled fish user config
+  echo_info "Setting up fish user config."
+  [[ $dryrun != true ]] && fish "$TARGET_DIR/.config/fish/run-once.fish"
+else
+  echo_err "Fish shell is required to install Fisher plugins. Skipping."
+fi
