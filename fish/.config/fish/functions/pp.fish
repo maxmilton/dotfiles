970aaa4a7201b6236a0cf05a1fce516a0ea521e7
Max Milton <max@wearegenki.com>
2018-04-19 08:46:34 +1000

    Make fish system upgrade function distro dynamic

--- fish/.config/fish/functions/pp.fish
+++ fish/.config/fish/functions/pp.fish
@@ -1,3 +1,14 @@
 function pp -d 'Get system package updates'
-  yaourt -Syu --aur $argv
+  DISTRO=(awk -F "=" '/^NAME/ {print $2}' /etc/os-release | tr -d '"')
+
+  switch $DISTRO
+    case "Arch Linux"
+      yaourt -Syu --aur $argv
+    case Fedora
+      sudo dnf update --refresh -y
+    case Ubuntu Debian
+      sudo apt update && sudo apt-get upgrade -y
+    case "Alpine Linux"
+      apk upgrade --update-cache
+  end
 end
