76f59c8ab0f62ef99162ab804b620831e8e93d3c
Max Milton <max@wearegenki.com>
2018-04-19 09:01:16 +1000

    Reorder case

--- fish/.config/fish/functions/pp.fish
+++ fish/.config/fish/functions/pp.fish
@@ -4,7 +4,7 @@ function pp -d 'Get system package updates'
       yaourt -Syu --aur $argv
     case Fedora
       sudo dnf update --refresh -y
-    case Ubuntu Debian
+    case Debian Ubuntu
       sudo apt update; and sudo apt-get upgrade -y
     case "Alpine Linux"
       apk upgrade --update-cache
