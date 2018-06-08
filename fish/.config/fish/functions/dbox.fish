70c4b047e40aa4fe5e06eb57642d75aca7eb0d93
Max Milton <max@wearegenki.com>
2018-06-09 07:04:08 +1000

    dbox improvements

--- fish/.config/fish/functions/dbox.fish
+++ fish/.config/fish/functions/dbox.fish
@@ -1,12 +1,15 @@
 function dbox -d 'Dockerized dev box with persistence'
   if docker container inspect dbox >/dev/null 2>&1
-    docker start -i dbox
+    if test (docker inspect -f '{{.State.Running}}' dbox) != 'true'
+      docker start dbox
+    end
+    docker exec -ti dbox entrypoint.sh $argv
   else
+    # no previous dbox container exists; first-time run
     docker run -ti \
       --name dbox \
-      --volume ~/Development/dotfiles/fish/dbox-entrypoint.sh:/usr/local/bin/entrypoint.sh \
-      --volume ~/Development/dotfiles/fish/.config/fish/onetime-config.fish:/config.fish \
+      --volume ~/Development/dotfiles/fish/dbox-entrypoint.sh:/usr/bin/entrypoint.sh \
       --entrypoint entrypoint.sh \
-      alpine:edge
+      alpine:edge $argv
   end
 end
--- fish/dbox-entrypoint.sh
+++ fish/dbox-entrypoint.sh
@@ -1,48 +1,96 @@
 #!/bin/sh
+set -euo pipefail
 
-set -e
-
-#
 # Bootstrap Dev Box Environment
 #
-# Docker container for quick experiments and protptyping. See dbox.fish for
-# init function.
+# Minimal docker container with persistence for quick experiments and
+# protptyping. Luncher script function in: ./config/fish/functions/dbox.fish
 #
 
-echo "  __  __"
-echo " /\\ \\/\\ \\"
-echo " \\_\\ \\ \\ \\____    ___   __  _"
-echo " /'_\` \\ \\ '__\`\\  / __\`\\/\\ \\/'\\"
-echo "/\\ \\L\\ \\ \\ \\L\\ \\/\\ \\L\\ \\/>  </"
-echo "\\ \\___,_\\ \\_,__/\\ \\____//\\_/\\_\\"
-echo " \\/__,_ /\\/___/  \\/___/ \\//\\/_/"
-echo ""
+cat <<'EOF'
+  __  __
+ /\ \/\ \
+ \_\ \ \ \____    ___   __  _
+ /'_` \ \ '__`\  / __`\/\ \/'\
+/\ \L\ \ \ \L\ \/\ \L\ \/>  </
+\ \___,_\ \_,__/\ \____//\_/\_\
+ \/__,_ /\/___/  \/___/ \//\/_/
 
-if ! command -v fish >/dev/null 2>&1; then
-  echo -e "\e[1;93mNew dbox, running first-time setup...\e[0m"
-  echo ""
+EOF
+
+reset='\e[0m'
+yellow_bold='\e[1;93m'
+
+if [ ! -f /first-run ]; then
+  echo -e "${yellow_bold}New dbox, running first-time setup...${reset}\\n"
+
+  # set dbox user
+  addgroup -g 6805 -S dbox
+  adduser -D -u 6805 -S -h /home/dbox -s /usr/bin/fish -G dbox dbox
+  addgroup dbox wheel
+
+  # allow dbox user to write to /dev/stderr
+  mkfifo -m 600 /tmp/logpipe
+  chown dbox:dbox /tmp/logpipe
+  cat <> /tmp/logpipe 1>&2 &
 
   # install base deps
   apk add --update \
     curl \
     fish \
-    git
+    git \
+    man \
+    mdocml-apropos \
+    sudo
+    # man-pages \
+    # less \
+    # less-doc \
+    # util-linux pciutils usbutils coreutils binutils findutils grep \
+
+  # no password sudo access
+  echo -e "%wheel\\tALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
+
+  mkdir -p ~/.config/fish
+
+  # write fish shell config file for root user
+  cat > ~/.config/fish/config.fish <<'EOF'
+set -U fish_path /home/dbox/.config/fish
+set -U fisher_config /home/dbox/.config/fisherman
+set -U fisher_cache /home/dbox/.cache/fisherman
+
+for file in $fish_path/conf.d/*.fish
+  builtin source $file 2> /dev/null
+end
+
+set fish_function_path $fish_path/functions $fish_function_path
+set fish_complete_path $fish_path/completions $fish_complete_path
+EOF
 
   # install fisherman
-  curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-
-  # install fisherman plugins
-  fish -c 'fisher MaxMilton/pure'
-
-  # precompile fish config
-  fish /config.fish
-
-  # reset $PATH additions
-  fish -c 'set -e fish_user_paths'
+  curl -Lo /home/dbox/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 
   # set up fish functions
-  git clone --depth 1 https://github.com/MaxMilton/dotfiles.git /usr/local/dotfiles
-  ln -s /usr/local/dotfiles/fish/.config/fish/functions/* /root/.config/fish/functions/
+  git clone --depth 1 https://github.com/MaxMilton/dotfiles.git /home/dbox/.dotfiles
+  ln -s /home/dbox/.dotfiles/fish/.config/fish/functions/* /home/dbox/.config/fish/functions/
+
+  # set correct permissions
+  chown -R dbox:dbox /home/dbox
+
+  # install fisherman plugins
+  sudo -u dbox fish -c 'fisher MaxMilton/pure'
+
+  # compile fish config
+  sudo -u dbox fish /home/dbox/.dotfiles/fish/.config/fish/onetime-config.fish
+
+  # change root shell
+  sed -i -- 's/root:\/bin\/ash/root:\/usr\/bin\/fish/' /etc/passwd
+
+  echo '0' > /first-run
 fi
 
-exec fish
+# allow docker '--user' argument or run as root if commands are passed in
+if [ "$(id -u)" != 0 ] || [ -n "$*" ]; then
+  exec fish -c "$*"
+fi
+
+exec sudo -u dbox -i
