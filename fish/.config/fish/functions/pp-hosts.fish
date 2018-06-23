dffb41e8b82c4cb2caf9b777302ca297a36a9a83
Max Milton <max@wearegenki.com>
2018-06-24 08:34:32 +1000

    Improve hosts update script

--- fish/.config/fish/functions/pp-hosts.fish
+++ fish/.config/fish/functions/pp-hosts.fish
@@ -4,20 +4,44 @@ function pp-hosts -d 'Update ad blocking hosts file'
     set -l TEMP_FILE /tmp/hosts
     set -l FILE /etc/dnsmasq.d/hosts
 
-    rm -f "$TEMP_FILE"
+    # clean up files from last run
+    rm -f "$TEMP_FILE" "$TEMP_FILE"-sorted
+
+    # download latest version of hosts file
     curl -fSL "$URL" -o "$TEMP_FILE"
 
-    # find which line is the end of custom local host entries
-    set -l line_number (grep -n '# End of custom host records.' "$TEMP_FILE" | cut -f1 -d:)
+    # find line number of end of top comments
+    set -l DIV '# ==============================================================='
+    set -l end_comments (grep -n "$DIV" "$TEMP_FILE" | cut -f1 -d:)
 
-    # comment out custom local host entries
-    sed -i "1,$line_number s/^/# /" "$TEMP_FILE"
+    # find line number of end of custom local host entries
+    set -l CUST_HOSTS '# End of custom host records.'
+    set -l end_cust_hosts (grep -n "$CUST_HOSTS" "$TEMP_FILE" | cut -f1 -d:)
 
+    # add header comments
+    head -n (math "$end_comments - 1") "$TEMP_FILE" > "$TEMP_FILE"-sorted
+
+    sed -i \
+      # remove custom local host entries
+      -e "$end_comments,$end_cust_hosts d" \
+      # remove comment lines
+      -e 's/#.*$//' \
+      # remove empty lines
+      -e '/^[[:space:]]*$/d' \
+      "$TEMP_FILE"
+
+    # add sorted list
+    sort -f -u "$TEMP_FILE" >> "$TEMP_FILE"-sorted
+
+    # ceate backup of previous file
     sudo rm -f "$FILE".bak
     sudo mv "$FILE" "$FILE".bak
-    sudo mv "$TEMP_FILE" "$FILE"
+
+    sudo mv "$TEMP_FILE"-sorted "$FILE"
+
+    # restart dnsmasq
     sudo systemctl restart dnsmasq.service
-    sleep 2
+    sleep 3
     sudo systemctl status dnsmasq.service
   end
 end
