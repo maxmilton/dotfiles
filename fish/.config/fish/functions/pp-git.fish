046c43e21d17f00e0f6538c6fedbba085a728080
Max Milton <max@wearegenki.com>
2018-07-29 07:52:27 +1000

    Improve git repos update script

--- fish/.config/fish/functions/pp-git.fish
+++ fish/.config/fish/functions/pp-git.fish
@@ -1,4 +1,15 @@
 function pp-git -d 'Update git repos'
-  # Run in sub-shell so the `cd` doesn't effect the active shell
-  fish -c "cd $HOME/Development/0__github_projects; and gll"
+  # update my forked repos
+  for dir in $HOME/Development/0__github_forks/*/
+    echo -e "\n\033[1;36m$dir\033[0m"
+    git -C $dir fetch upstream
+    and git -C $dir merge upstream/master master
+    and git -C $dir push origin master
+  end
+
+  # update cloned repos
+  for dir in $HOME/Development/0__github_clones/*/
+    echo -e "\n\033[1;36m$dir\033[0m"
+    git -C $dir pull --prune
+  end
 end
