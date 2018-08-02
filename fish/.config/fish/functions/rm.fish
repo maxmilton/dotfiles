92578c42707269d89f6553129a1d3ec8142b18ee
Max Milton <mamilton@seven.com.au>
2018-08-02 13:34:09 +1000

    Add rm workaround for Alpine Linux

--- fish/.config/fish/functions/rm.fish
+++ fish/.config/fish/functions/rm.fish
@@ -1,7 +1,14 @@
 function rm -d 'Move files into trash'
   if test (uname -s) != 'Darwin'
     # Linux
-    gio trash $argv
+    set -l distro (awk -F "=" '/^NAME/ {print $2}' /etc/os-release | tr -d '"')
+
+    if test $distro = 'Alpine Linux'
+      # delete immediately since Alpine is almost always CLI only
+      rm $argv
+    else
+      gio trash $argv
+    end
   else
     # macOS
     rmtrash $argv
