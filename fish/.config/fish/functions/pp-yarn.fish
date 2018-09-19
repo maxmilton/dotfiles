437ed0070204d93ef4a7472aa602fc0e226cd13c
Max Milton <max@wearegenki.com>
2018-09-20 07:47:48 +1000

    Improve yarn global upgrade fish function

--- fish/.config/fish/functions/pp-yarn.fish
+++ fish/.config/fish/functions/pp-yarn.fish
@@ -1,5 +1,9 @@
 function pp-yarn -d 'Update global yarn packages'
   if type -q yarn
+    # run install first in case `package.json` has changed
+    begin; cd "$HOME"/.config/yarn/global; and yarn install; end
+
+    # do the actual upgrades
     yarn global upgrade --save
   end
 end
