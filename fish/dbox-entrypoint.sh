4c6eeccf19838867fbd7f635d1b820367c3d0d0c
Max Milton <mamilton@seven.com.au>
2018-09-26 12:04:10 +1000

    Add glibc to dbox but commented out

--- fish/dbox-entrypoint.sh
+++ fish/dbox-entrypoint.sh
@@ -56,6 +56,12 @@ if [ ! -f /first-run ]; then
     # XXX: If busybox built-ins are not enough, install:
     # util-linux pciutils usbutils coreutils binutils findutils grep
 
+  # install glibc
+  # apk add ca-certificates wget
+  # wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
+  # wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.28-r0/glibc-2.28-r0.apk
+  # apk add glibc-*.apk
+
   # no password sudo access
   echo -e "%wheel\\tALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
 
