9f2c738802a3befe9c266ad07e9ab785fb954531
Max Milton <mamilton@seven.com.au>
2018-10-06 07:04:36 +1000

    Comppletely refactor fish gz function

--- fish/.config/fish/functions/gz.fish
+++ fish/.config/fish/functions/gz.fish
@@ -1,36 +1,54 @@
 function gz -d 'Compare gzip\'d sizes of a file'
-  echo -e "\e[93moriginal (bytes / kilobytes / percent): \e[0m"
-  set orig (wc -c < "$argv[1]")
-  set sizek (math -s2 "$orig / 1024")
-  echo "$orig B / $sizek K"
+  cat $argv ^/dev/null | tr -d '\n' | read -l input
+  set -ql input; or set -l input $argv[1]
 
-  echo -e "\e[91mgzip -1: \e[0m"
-  set size (gzip -c -1 "$argv[1]" | wc -c)
-  set sizek (math -s2 "$size / 1024")
-  set percent (math -s2 "($size / $orig) * 100")
-  echo "$size B / $sizek K / $percent %"
+  # colours
+  set -l normal (set_color normal)
+  set -l grey (set_color brblack)
+  set -l yellow (set_color bryellow)
+  set -l red (set_color brred)
 
-  echo -e "\e[91mgzip -5: \e[0m"
-  set size (gzip -c -5 "$argv[1]" | wc -c)
-  set sizek (math -s2 "$size / 1024")
-  set percent (math -s2 "($size / $orig) * 100")
-  echo "$size B / $sizek K / $percent %"
+  # table parts
+  set -l divY "$grey│$yellow"
+  set -l divN "$grey│$normal"
 
-  echo -e "\e[91mgzip -6: \e[0m"
-  set size (gzip -c -6 "$argv[1]" | wc -c)
-  set sizek (math -s2 "$size / 1024")
-  set percent (math -s2 "($size / $orig) * 100")
-  echo "$size B / $sizek K / $percent %"
+  # original file
+  set -l orig (echo "$input" | wc -c)
+  set -l sizek1 (math -s2 "$orig / 1024")
 
-  echo -e "\e[91mgzip -9: \e[0m"
-  set size (gzip -c -9 "$argv[1]" | wc -c)
-  set sizek (math -s2 "$size / 1024")
-  set percent (math -s2 "($size / $orig) * 100")
-  echo "$size B / $sizek K / $percent %"
+  # gzip -6
+  set -l start2 (date '+%s%3N')
+  set -l size2 (echo "$input" | gzip -cf -6 | wc -c)
+  set -l stop2 (date '+%s%3N')
+  set -l duration2 (math "$stop2 - $start2")
+  set -l sizek2 (math -s2 "$size2 / 1024")
+  # set -l percent2 (math -s2 "($size2 / $orig) * 100")
+  set -l percent2 (math -s2 "($size2 / $orig) * 100")
 
-  echo -e "\e[91mbrotli -9: \e[0m"
-  set size (brotli -c -9 "$argv[1]" | wc -c)
-  set sizek (math -s2 "$size / 1024")
-  set percent (math -s2 "($size / $orig) * 100")
-  echo "$size B / $sizek K / $percent %"
+  # gzip -9
+  set -l start3 (date '+%s%3N')
+  set -l size3 (echo "$input" | gzip -cf -9 | wc -c)
+  set -l stop3 (date '+%s%3N')
+  set -l duration3 (math "$stop3 - $start3")
+  set -l sizek3 (math -s2 "$size3 / 1024")
+  set -l percent3 (math -s2 "($size3 / $orig) * 100")
+
+  # brotli -9
+  set -l start4 (date '+%s%3N')
+  set -l size4 (echo "$input" | brotli -cf -9 | wc -c)
+  set -l stop4 (date '+%s%3N')
+  set -l duration4 (math "$stop4 - $start4")
+  set -l sizek4 (math -s2 "$size4 / 1024")
+  set -l percent4 (math -s2 "($size4 / $orig) * 100")
+
+  echo ""
+  echo -e $yellow "source    $divY B     $divY KB    $divY %   $divY time"
+  echo -e $grey "──────────┼───────┼───────┼─────┼──────"
+  echo -e $red "original " $divN $orig $divN $sizek1 $divN 100
+  echo -e $red "gzip -6   $divN $size2  $divN $sizek2  $divN $percent2 $divN $duration2 ms"
+  echo -e $red "gzip -9   $divN $size3  $divN $sizek3  $divN $percent3 $divN $duration3 ms"
+  echo -e $red "brotli -9 $divN $size4  $divN $sizek4  $divN $percent4 $divN $duration4 ms"
+
+  # reset colour
+  echo -en $normal
 end
