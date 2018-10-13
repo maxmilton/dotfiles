791ed121a6e20c6570fb99adc390a44df4a9df9f
Max Milton <max@wearegenki.com>
2018-10-14 06:56:19 +1100

    Better VS Code pre install

--- vscode/pre-install.sh
+++ vscode/pre-install.sh
@@ -3,23 +3,34 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # TODO: Change this once the new dictionary project is ready.
-# DICTIONARY_DIR=${DICTIONARY_DIR:-"$TARGET_DIR/Development/packages/dictionary"}
-DICTIONARY_DIR=${DICTIONARY_DIR:-"$TARGET_DIR/Development/hunspell-dictionary"}
+# DICTIONARY_DIR=${DICTIONARY_DIR:-"$TARGET_DIR""/Development/packages/dictionary"}
+DICTIONARY_DIR=${DICTIONARY_DIR:-"$TARGET_DIR""/Development/hunspell-dictionary"}
 
 if [[ "$OS" != 'Darwin' ]]; then
-  $cmd mkdir -v -p "$TARGET_DIR/.config/Code/User"
+  CODE_DIR="$TARGET_DIR"/.config/Code
+  INSIDERS_DIR="$TARGET_DIR""/.config/Code - Insiders"
+
+  $cmd mkdir $verbosity -p "$CODE_DIR"/User
 
   # dictionaries
-  if [[ ! -d "$TARGET_DIR/.config/Code/Dictionaries" ]]; then
+  if [[ ! -d "$CODE_DIR/Dictionaries" ]]; then
     if [[ ! -d "/usr/share/hunspell" ]]; then
       if [[ $DISTRO = "Fedora" ]]; then
-        $cmd sudo ln -v -s /usr/share/myspell /usr/share/hunspell
+        $cmd sudo ln $verbosity -sf /usr/share/myspell /usr/share/hunspell
       else
-        $cmd sudo mkdir -v -p /usr/share/hunspell
+        $cmd sudo mkdir $verbosity -p /usr/share/hunspell
       fi
     fi
-    $cmd sudo ln -i -v -s $DICTIONARY_DIR/*.{dic,aff} /usr/share/hunspell
-    $cmd sudo ln -v -s /usr/share/hunspell "$TARGET_DIR/.config/Code/Dictionaries"
+    $cmd sudo ln -i $verbosity -sf "$DICTIONARY_DIR"/*.{dic,aff} /usr/share/hunspell
+    $cmd sudo ln $verbosity -s /usr/share/hunspell "$CODE_DIR"/Dictionaries
+  fi
+
+  # VS Code insiders (only if dir exists)
+  if [[ -d "$INSIDERS_DIR" ]]; then
+    $cmd ln $verbosity -sf "$CODE_DIR"/Dictionaries "$INSIDERS_DIR"
+    $cmd ln $verbosity -sf "$CODE_DIR"/User/settings.json "$INSIDERS_DIR"/User
+    $cmd ln $verbosity -sf "$CODE_DIR"/User/keybindings.json "$INSIDERS_DIR"/User
+    $cmd ln $verbosity -sf "$CODE_DIR"/User/snippets/*.json "$INSIDERS_DIR"/User/snippets
   fi
 else
   echo_warn "You need to manually set up VS Code directories on macOS."
