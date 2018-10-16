f8e23ab44abbdd2b354e4ba13bea7006856728e2
Max Milton <max@wearegenki.com>
2018-10-16 12:55:23 +1100

    Add check to gce update fish function

--- fish/.config/fish/functions/up_gce.fish
+++ fish/.config/fish/functions/up_gce.fish
@@ -1,3 +1,7 @@
 function up_gce -d 'Update Google Cloud SDK'
-  yes | gcloud components update
+  if type -q gcloud
+    yes | gcloud components update
+  else
+    echo "Google Cloud SDK not installed"
+  end
 end
