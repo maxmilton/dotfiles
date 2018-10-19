cb2145bcb42ad836b0207ee4421deab6b2ce80ec
Max Milton <max@wearegenki.com>
2018-10-19 13:19:30 +1100

    Disable proxy in fish y function

--- fish/.config/fish/functions/y.fish
+++ fish/.config/fish/functions/y.fish
@@ -1,4 +1,10 @@
 function y -d 'Run yarn dev script (default) or another script'
+  # disable proxy (squid via net-cache in docker)
+  set HTTP_PROXY ''
+  set HTTPS_PROXY ''
+  set http_proxy ''
+  set https_proxy ''
+
   if test -z $argv[1]
     yarn run start; or yarn run dev
   else
