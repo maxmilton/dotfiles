c5a905de63aa6338fdb1f58dfacdd4415341080b
Max Milton <max@wearegenki.com>
2018-10-19 13:21:30 +1100

    Set env as local

--- fish/.config/fish/functions/y.fish
+++ fish/.config/fish/functions/y.fish
@@ -1,9 +1,9 @@
 function y -d 'Run yarn dev script (default) or another script'
   # disable proxy (squid via net-cache in docker)
-  set HTTP_PROXY ''
-  set HTTPS_PROXY ''
-  set http_proxy ''
-  set https_proxy ''
+  set --local HTTP_PROXY ''
+  set --local HTTPS_PROXY ''
+  set --local http_proxy ''
+  set --local https_proxy ''
 
   if test -z $argv[1]
     yarn run start; or yarn run dev
