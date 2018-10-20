bb8bb24eab7c9ba677908aa65550cb3967d28d6a
Max Milton <max@wearegenki.com>
2018-10-21 10:05:44 +1100

    Fix directory changing in yarn update fish script

--- fish/.config/fish/functions/up_yarn.fish
+++ fish/.config/fish/functions/up_yarn.fish
@@ -1,11 +1,19 @@
 function up_yarn -d 'Update global yarn packages'
   if type --quiet yarn
-    # run install first in case `package.json` has changed
-    begin; cd "$HOME"/.config/yarn/global; and yarn install; end
+    set --local yarn_global_dir "$HOME"/.config/yarn/global
 
-    # do the actual upgrades
-    yarn global upgrade --save &
-    set --local PID %last
-    echo "Updating yarn global packages in background with PID" "$PID"
+    if test -d $yarn_global_dir
+      pushd $yarn_global_dir
+      # do install to update `yarn.lock` in case `package.json` has changed
+      yarn install
+      popd
+
+      # do the actual upgrades
+      yarn global upgrade --save &
+      set --local PID %last
+      echo "Updating yarn global packages in background with PID" "$PID"
+    else
+      echo "Yarn global directory not found;" "$yarn_global_dir"
+    end
   end
 end
