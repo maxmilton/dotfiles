d834e7d8f09b7a823526fd36f40b2a97e69d58d5
Max Milton <max@wearegenki.com>
2019-01-29 07:26:50 +1100

    Improve post install script

--- post-install.sh
+++ post-install.sh
@@ -30,12 +30,49 @@ if ! hash chezmoi 2>/dev/null; then
   exit 1
 fi
 
+###################
+# System packages #
+###################
+
+# TODO: Install themer packages + set up gen output
+
+# Install common system packages
+
+echo OS $os
+echo DISTRO $distro
+
+if [ "$distro" = 'Arch Linux' ]; then
+  # update package database
+  sudo pacman -Sy
+  # isntall packages if missing
+  sudo pacman -S --needed \
+    bat \
+    docker \
+    exa \
+    fd \
+    fish \
+    # flatpak \
+    # fzf \
+    fzy \
+    git \
+    ripgrep \
+    tmux \
+    vim
+elif [ "$distro" = 'Ubuntu' ]; then
+  # sudo apt update
+  # sudo apt install -y fish vim git ripgrep fd fzf docker
+fi
+exit1
+
 ########
 # Fish #
 ########
 
 # check fish shell is installed
 if hash fish 2>/dev/null; then
+  # generate compiled fish user config
+  fish "$target_dir"/.config/fish/oneshot-config.fish
+
   # check fisher is installed
   if ! fish -c 'functions -q fisher' 2>/dev/null; then
     echo_warn "Fisher not found, downloading..."
@@ -43,10 +80,8 @@ if hash fish 2>/dev/null; then
     curl https://git.io/fisher --create-dirs -sLo "$XDG_CONFIG_HOME"'/fish/functions/fisher.fish'
   fi
 
+  # initialise fisher and run update packages
   fish -c fisher
-
-  # generate compiled fish user config
-  fish "$target_dir"/.config/fish/oneshot-config.fish
 else
   echo_err 'Fish shell is required to install user fish config. Skipping.'
 fi
@@ -55,9 +90,11 @@ fi
 # VIM #
 #######
 
-mkdir -v -p "$target_dir"/.vim/backup
-mkdir -v -p "$target_dir"/.vim/swap
-mkdir -v -p "$target_dir"/.vim/undo
+mkdir -vp "$target_dir"/.vim/backup
+mkdir -vp "$target_dir"/.vim/swap
+mkdir -vp "$target_dir"/.vim/undo
+
+# TODO: Automatically run `PlugInstall` in vim
 
 ###########
 # VS Code #
@@ -67,45 +104,45 @@ if [ "$os" = Darwin ]; then
   echo_warn 'You need to manually set up VS Code directories on macOS.'
 else
   hunspell_dir=/usr/share/hunspell
-  dictionary_dir="$target_dir"/Projects/packages/dictionary
+  dictionary_dir="$target_dir"/Projects/packages/dictionary/dist
   code_dir="$target_dir"/.config/Code
   insiders_dir="$target_dir"'/.config/Code - Insiders'
 
-  mkdir -v -p "$code_dir"/User/snippets
+  mkdir -vp "$code_dir"/User/snippets
 
   # dictionaries
   if [ ! -d "$code_dir"/Dictionaries ]; then
     if [ ! -d "$hunspell_dir" ]; then
       if [ "$distro" = Fedora ]; then
-        sudo ln -v -s /usr/share/myspell "$hunspell_dir"
+        sudo ln -vs /usr/share/myspell "$hunspell_dir"
       else
-        sudo mkdir -v -p "$hunspell_dir"
+        sudo mkdir -vp "$hunspell_dir"
       fi
     fi
 
     if [ ! -d "$hunspell_dir" ]; then
-      sudo ln -i -v -sf "$dictionary_dir"/en_GB.dic "$hunspell_dir"
-      sudo ln -i -v -sf "$dictionary_dir"/en_GB.aff "$hunspell_dir"
+      sudo ln -i -vsf "$dictionary_dir"/en_GB.dic "$hunspell_dir"
+      sudo ln -i -vsf "$dictionary_dir"/en_GB.aff "$hunspell_dir"
     fi
     if [ ! -d "$code_dir"/Dictionaries ]; then
-      sudo ln -v -s "$hunspell_dir" "$code_dir"/Dictionaries
+      sudo ln -vs "$hunspell_dir" "$code_dir"/Dictionaries
     fi
   fi
 
   # VS Code insiders
   if type code-insiders > /dev/null 2>&1; then
-    mkdir -v -p "$insiders_dir"/User/snippets
+    mkdir -vp "$insiders_dir"/User/snippets
     if [ ! -d "$insiders_dir"/Dictionaries ]; then
-      ln -v -s "$hunspell_dir" "$insiders_dir"/Dictionaries
+      ln -vs "$hunspell_dir" "$insiders_dir"/Dictionaries
     fi
-    if [ ! -d "$insiders_dir"/User/settings.json ]; then
-      ln -v -s "$code_dir"/User/settings.json "$insiders_dir"/User
+    if [ ! -f "$insiders_dir"/User/settings.json ]; then
+      ln -vs "$code_dir"/User/settings.json "$insiders_dir"/User
     fi
-    if [ ! -d "$insiders_dir"/User/keybindings.json ]; then
-      ln -v -s "$code_dir"/User/keybindings.json "$insiders_dir"/User
+    if [ ! -f "$insiders_dir"/User/keybindings.json ]; then
+      ln -vs "$code_dir"/User/keybindings.json "$insiders_dir"/User
     fi
     if [ ! -d "$insiders_dir"/User/snippets ]; then
-      ln -v -s "$code_dir"/User/snippets "$insiders_dir"/User
+      ln -vs "$code_dir"/User/snippets "$insiders_dir"/User
     fi
   fi
 fi
