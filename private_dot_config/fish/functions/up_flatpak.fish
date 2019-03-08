1a63b055095c0a10d9a29617f3ada64db889c3c0
Max Milton <max@wearegenki.com>
2019-03-09 03:25:03 +1100

    Experimental improvement to flatpak function

--- private_dot_config/fish/functions/up_flatpak.fish
+++ private_dot_config/fish/functions/up_flatpak.fish
@@ -1,6 +1,7 @@
 function up_flatpak -d 'Update flatpak packages'
   if type --quiet flatpak
-    flatpak update --noninteractive --assumeyes
+    # flatpak update --noninteractive --assumeyes
+    flatpak update
     flatpak list
   end
 end
