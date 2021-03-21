81110d597286ec2616218fea05815f4c2c0b6f84
Max Milton <max@wearegenki.com>
2021-03-22 07:37:03 +1000

    Clean up fish function

--- private_dot_config/fish/functions/kill_process.fish
+++ private_dot_config/fish/functions/kill_process.fish
@@ -1,21 +1,19 @@
-# TODO: Add multi-select once availiable in fzy -- https://github.com/jhawthorn/fzy/pull/124
-
 function kill_process --description 'Kill processes'
-  set -l __kp__pid ''
+  set -l __kp_pid ''
 
   if contains -- '--tcp' $argv
-    set __kp__pid (lsof -Pwni tcp | sed 1d | fzy --lines 20 | awk '{print $2}')
+    set __kp_pid (lsof -Pwni tcp | sed 1d | fzy --lines 20 | awk '{print $2}')
   else
-    set __kp__pid (ps -ef | sed 1d | fzy --lines 20 | awk '{print $2}')
+    set __kp_pid (ps -ef | sed 1d | fzy --lines 20 | awk '{print $2}')
   end
 
-  set -l __kp__kc $argv[1]
+  set -l __kp_kc $argv[1]
 
-  if test "x$__kp__pid" != "x"
+  if test "x$__kp_pid" != "x"
     if test "x$argv[1]" != "x"
-      echo $__kp__pid | xargs kill $argv[1]
+      echo $__kp_pid | xargs kill $argv[1]
     else
-      echo $__kp__pid | xargs kill -9
+      echo $__kp_pid | xargs kill -9
     end
 
     kill_process
