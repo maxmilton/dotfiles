7722ff22e3fb7b9be2bb98c7d0017b919dd39bfe
Max Milton <max@wearegenki.com>
2021-04-15 17:51:36 +1000

    Allow access to cwd file in gcloud

--- private_dot_config/fish/functions/gcloud.fish
+++ private_dot_config/fish/functions/gcloud.fish
@@ -1,5 +1,5 @@
 # https://github.com/MaxMilton/dockerfiles/tree/master/gcloud
 
 function gcloud --description 'Google Cloud SDK'
-  docker run --rm -ti --volumes-from gcloud-config local/gcloud gcloud $argv
+  docker run --rm -ti --volumes-from gcloud-config --volume="$PWD":/data local/gcloud gcloud $argv
 end
