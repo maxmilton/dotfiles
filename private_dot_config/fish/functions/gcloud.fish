e5a73b904f98a0f013d134776c0ea07515100d35
Max Milton <max@wearegenki.com>
2021-05-16 17:18:44 +1000

    Better gcloud fish function

--- private_dot_config/fish/functions/gcloud.fish
+++ private_dot_config/fish/functions/gcloud.fish
@@ -1,5 +1,10 @@
 # https://github.com/MaxMilton/dockerfiles/tree/master/gcloud
 
 function gcloud --description 'Google Cloud SDK'
+  # Set up auth on first run
+  if test docker container inspect gcloud-config >/dev/null 2>&1
+    docker run -ti --name gcloud-config local/gcloud gcloud auth login
+  end
+
   docker run --rm -ti --volumes-from gcloud-config --volume="$PWD":/data local/gcloud gcloud $argv
 end
