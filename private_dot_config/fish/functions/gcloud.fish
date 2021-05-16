ab385f3835634537ad77f4fd74941a9933e67e10
Max Milton <max@wearegenki.com>
2021-05-16 17:29:03 +1000

    Fix function logic

--- private_dot_config/fish/functions/gcloud.fish
+++ private_dot_config/fish/functions/gcloud.fish
@@ -2,7 +2,8 @@
 
 function gcloud --description 'Google Cloud SDK'
   # Set up auth on first run
-  if test docker container inspect gcloud-config >/dev/null 2>&1
+  docker container inspect gcloud-config >/dev/null 2>&1
+  if test $status -ne 0
     docker run -ti --name gcloud-config local/gcloud gcloud auth login
   end
 
