a749c73755289f410b15be35c4b0378b81ebee46
Max Milton <max@wearegenki.com>
2021-05-25 14:31:29 +1000

    Tweak spacing

--- private_dot_config/fish/functions/gcloud.fish
+++ private_dot_config/fish/functions/gcloud.fish
@@ -7,5 +7,10 @@ function gcloud --description 'Google Cloud SDK'
     docker run -ti --name gcloud-config local/gcloud gcloud auth login
   end
 
-  docker run --rm -ti --volumes-from gcloud-config --volume="$PWD":/data local/gcloud gcloud $argv
+  docker run \
+    --rm \
+    -ti \
+    --volumes-from gcloud-config \
+    --volume="$PWD":/data \
+    local/gcloud gcloud $argv
 end
