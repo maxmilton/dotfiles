6e746064f7fb1b7bf2dba0d12b51cfbb56e49879
Max Milton <max@wearegenki.com>
2021-05-26 14:16:48 +1000

    Use lowercase for local fish vars

--- private_dot_config/fish/functions/up_hosts.fish
+++ private_dot_config/fish/functions/up_hosts.fish
@@ -1,26 +1,26 @@
 function up_hosts --description 'Update dnsmasq blacklist hosts file'
   if type --query dnsmasq
     if systemctl is-enabled dnsmasq.service 2>/dev/null
-      set --local URL https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts
-      set --local TEMP_FILE /tmp/hosts
-      set --local FILE /etc/dnsmasq.d/hosts
+      set --local url https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts
+      set --local temp_file /tmp/hosts
+      set --local hosts_file /etc/dnsmasq.d/hosts
 
       # clean up files from last run
-      rm -f "$TEMP_FILE" "$TEMP_FILE"-sorted
+      rm -f "$temp_file" "$temp_file"-sorted
 
       # download latest version of hosts file
-      curl -fSL "$URL" -o "$TEMP_FILE"
+      curl -fSL "$url" -o "$temp_file"
 
       # find line number of end of top comments
-      set --local DIV '# ==============================================================='
-      set --local end_comments (grep -n "$DIV" "$TEMP_FILE" | cut -f1 -d:)
+      set --local div '# ==============================================================='
+      set --local end_comments (grep -n "$div" "$temp_file" | cut -f1 -d:)
 
       # find line number of end of custom local host entries
-      set --local CUST_HOSTS '# End of custom host records.'
-      set --local end_cust_hosts (grep -n "$CUST_HOSTS" "$TEMP_FILE" | cut -f1 -d:)
+      set --local cust_hosts_marker '# End of custom host records.'
+      set --local end_cust_hosts (grep -n "$cust_hosts_marker" "$temp_file" | cut -f1 -d:)
 
       # add header comments
-      head -n (math "$end_comments - 1") "$TEMP_FILE" > "$TEMP_FILE"-sorted
+      head -n (math "$end_comments - 1") "$temp_file" > "$temp_file"-sorted
 
       sed -i \
         # remove custom local host entries
@@ -32,16 +32,16 @@ function up_hosts --description 'Update dnsmasq blacklist hosts file'
         # remove broken domains (otherwise dnsmasq won't start)
         -e '/^0.0.0.0 -/d' \
         -e '/--/d' \
-        "$TEMP_FILE"
+        "$temp_file"
 
       # add sorted list
-      sort -f -u "$TEMP_FILE" >> "$TEMP_FILE"-sorted
+      sort -f -u "$temp_file" >> "$temp_file"-sorted
 
       # ceate backup of previous file
-      sudo rm -f "$FILE".bak
-      sudo mv "$FILE" "$FILE".bak
+      sudo rm -f "$hosts_file".bak
+      sudo mv "$hosts_file" "$hosts_file".bak
 
-      sudo mv "$TEMP_FILE"-sorted "$FILE"
+      sudo mv "$temp_file"-sorted "$hosts_file"
 
       # restart dnsmasq
       sudo systemctl restart dnsmasq.service
