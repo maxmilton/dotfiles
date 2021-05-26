3283a67fca74fe2108c1f25087ffe59740065c64
Max Milton <max@wearegenki.com>
2021-05-26 14:03:28 +1000

    Refactor fish gg

--- private_dot_config/fish/functions/gg.fish
+++ private_dot_config/fish/functions/gg.fish
@@ -1,9 +1,14 @@
-function gchkout --description 'Switch master🔁dev or checkout branch (create if not exists)'
+function gg --description 'Switch git master🔁dev/next or checkout branch (+ create if not exists)'
   if test -z $argv[1]
-    if test (git symbolic-ref --short HEAD) = "master"
-      git checkout dev
-    else
+    if test (git show-ref --hash --verify --head HEAD) != (git show-ref --hash refs/heads/master)
       git checkout master
+    else if git show-ref --verify --quiet refs/heads/dev
+      git checkout dev
+    else if git show-ref --verify --quiet refs/heads/next
+      git checkout next
+    else
+      echo 'Error: No local git branch named "dev" or "next" exist'
+      return 1
     end
   else
     git checkout -b $argv 2> /dev/null; or git checkout $argv
