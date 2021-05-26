25183142677fc3f80c78c2347e9e9e24e6576c75
Max Milton <max@wearegenki.com>
2021-05-26 14:15:46 +1000

    Delete fish functions I never use

--- private_dot_config/fish/functions/dti.fish
+++
@@ -1,3 +0,0 @@
-function dti --description 'list all tags for a Docker image on a remote registry'
-  curl -s 'https://registry.hub.docker.com/v1/repositories/'$argv[1]'/tags' | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}'
-end
--- private_dot_config/fish/functions/git_info.fish
+++
@@ -1,6 +0,0 @@
-function git_info --description 'Print information about a git repo'
-  set -l dir $argv[1]
-  set -l reset (set_color normal)
-  echo -s (set_color --bold cyan) '📂 ' $dir $reset
-  echo -s (set_color yellow) '🔖 ' (git -C $dir rev-parse --abbrev-ref HEAD) $reset
-end
