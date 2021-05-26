eeb22b43cdd58c3b3eabc2717e2ceef80d6eb284
Max Milton <max@wearegenki.com>
2021-05-26 14:16:59 +1000

    Rename fish var

--- private_dot_config/fish/functions/kill_process.fish
+++ private_dot_config/fish/functions/kill_process.fish
@@ -1,19 +1,17 @@
 function kill_process --description 'Kill processes'
-  set -l __kp_pid ''
+  set -l proc_pid ''
 
   if contains -- '--tcp' $argv
-    set __kp_pid (lsof -Pwni tcp | sed 1d | fzy --lines 20 | awk '{print $2}')
+    set proc_pid (lsof -Pwni tcp | sed 1d | fzy --lines 20 | awk '{print $2}')
   else
-    set __kp_pid (ps -ef | sed 1d | fzy --lines 20 | awk '{print $2}')
+    set proc_pid (ps -ef | sed 1d | fzy --lines 20 | awk '{print $2}')
   end
 
-  set -l __kp_kc $argv[1]
-
-  if test "x$__kp_pid" != "x"
+  if test "x$proc_pid" != "x"
     if test "x$argv[1]" != "x"
-      echo $__kp_pid | xargs kill $argv[1]
+      echo $proc_pid | xargs kill $argv[1]
     else
-      echo $__kp_pid | xargs kill -9
+      echo $proc_pid | xargs kill -9
     end
 
     kill_process
