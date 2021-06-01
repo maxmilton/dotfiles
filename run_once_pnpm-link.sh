5db6b5a6a824aec62cdae6682e139747da0434c9
Max Milton <max@wearegenki.com>
2021-06-01 11:34:04 +1000

    Fix pnpm package.json link

--- run_once_pnpm-link.sh
+++ run_once_pnpm-link.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 # Link global pnpm with home so it can be managed be chezmoi
-sudo ln "$HOME"/.config/pnpm/package.json /usr/pnpm-global/5/package.json
+sudo ln -s "$HOME"/.config/pnpm/package.json /usr/pnpm-global/5/package.json
