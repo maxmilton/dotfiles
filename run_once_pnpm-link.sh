91ff3164e425f9f7a7531f7c1ffbb89a8acb2e69
Max Milton <max@wearegenki.com>
2021-09-04 07:18:23 +1000

    Fix pnpm link script when package.json already exists

--- run_once_pnpm-link.sh
+++ run_once_pnpm-link.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 # Link global pnpm with home so it can be managed be chezmoi
 sudo mkdir -p /usr/pnpm-global/5
+sudo rm -f /usr/pnpm-global/5/package.json
 sudo ln -s "$HOME"/.config/pnpm/package.json /usr/pnpm-global/5/package.json
