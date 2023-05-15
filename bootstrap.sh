47afd0afba31d745b8222be4145ea327f8fbeb48
Max Milton <max@happyhackers.xyz>
2023-05-15 16:04:54 +1000

    Add bins

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,10 @@ script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 mkdir -vp ~/.config/fish/functions
 mkdir -vp ~/.config/helix
 mkdir -vp ~/.gnupg
+mkdir -vp ~/.local/bin
 mkdir -vp ~/.ssh
 
-# export SIMPLE_BACKUP_SUFFIX=.bak
+export SIMPLE_BACKUP_SUFFIX=.bak
 
 ln -vbs "${script_dir}/.config/electron-flags.conf" ~/.config/electron-flags.conf
 ln -vbs "${script_dir}/.config/fish/config.fish" ~/.config/fish/config.fish
@@ -24,6 +25,8 @@ ln -vbs "${script_dir}/.config/helix/languages.toml" ~/.config/helix/languages.t
 ln -vbs "${script_dir}/.config/zls.json" ~/.config/zls.json
 ln -vbs "${script_dir}/.gnupg/gpg-agent.conf" ~/.gnupg/gpg-agent.conf
 ln -vbs "${script_dir}/.gnupg/gpg.conf" ~/.gnupg/gpg.conf
+# ln -vbs "${script_dir}/.local/bin/sudo" ~/.local/bin/sudo
+ln -vbs "${script_dir}/.local/bin/zigup" ~/.local/bin/zigup
 ln -vbs "${script_dir}/.ssh/config" ~/.ssh/config
 ln -vbs "${script_dir}/.curlrc" ~/.curlrc
 ln -vbs "${script_dir}/.gitconfig" ~/.gitconfig
