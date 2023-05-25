c22cab49d2efd2432d4446ea0e0ad3dd73fef1bd
Max Milton <max@happyhackers.xyz>
2023-05-25 21:24:04 +1000

    feat: Add script to disable unnecessary gnome services

---
+++ .local/bin/disable-gnome-services.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -eu
+systemctl --user mask org.gnome.SettingsDaemon.A11ySettings.service
+systemctl --user mask org.gnome.SettingsDaemon.PrintNotifications.service
+systemctl --user mask org.gnome.SettingsDaemon.ScreensaverProxy.service
+systemctl --user mask org.gnome.SettingsDaemon.Smartcard.service
+systemctl --user mask org.gnome.SettingsDaemon.Wacom.service
+
+#sudo systemctl mask bolt
+sudo systemctl mask geoclue.service
+
+# gsettings set org.gnome.system.location enabled false
