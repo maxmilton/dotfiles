af3b273ff80eb3c4b2c5f0efaf2600314018e851
Max Milton <max@happyhackers.xyz>
2023-06-06 08:02:51 +1000

    bug: Add missing home dir on sysboot

--- .local/bin/sysboot.sh
+++ .local/bin/sysboot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -eu
 # Btrfs only; create snapshot of root filesystem and boot an ephemeral instance
 sudo btrfs subvolume show / >/dev/null || exit 1
-sudo systemd-nspawn -D / -xb
+sudo systemd-nspawn -D / -xb --overlay="$HOME"
