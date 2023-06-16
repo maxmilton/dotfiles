2bd880769d99ee1357d4d4554b48d6389d7de58c
Max Milton <max@happyhackers.xyz>
2023-06-17 08:47:29 +1000

    feat: Add lutris script

---
+++ .local/bin/lutris.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -eu
+sudo systemd-nspawn \
+  --bind-ro=/run/user/1000/pulse/native:/run/user/host/pulse/native \
+  --bind-ro=/run/user/1000/wayland-0:/run/user/host/wayland-0 \
+  --bind=/dev/dri/card0 \
+  --bind=/dev/dri/renderD128 \
+  --bind=/tmp/.X11-unix/X0 \
+  --bind="$XAUTHORITY":/home/max/.Xauthority \
+  --bind=/home/max/Downloads \
+  --bind-ro=/home/max/Projects \
+  --directory=/var/lib/machines/lutris \
+  --boot
