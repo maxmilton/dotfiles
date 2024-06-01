ec9fd0b65f541c44e3840bd8908aed437a785efa
Max Milton <max@happyhackers.xyz>
2024-06-02 06:42:52 +0900

    chore: Improve fish dev init script

--- .config/fish/config-oneshot-dev.fish
+++ .config/fish/config-oneshot-dev.fish
@@ -12,22 +12,40 @@ source "$DIR"/config-oneshot.fish
 set -U hydro_color_prompt magenta
 
 # macOS
-if test (uname -s) = "Darwin"
-  fish_add_path "$HOME"/.nix-profile/bin /nix/var/nix/profiles/default/bin
+if test (uname -s) = "Darwin" and test -d "$HOME"/.nix-profile/bin
+    fish_add_path "$HOME"/.nix-profile/bin /nix/var/nix/profiles/default/bin
 end
 
 # Set user paths in reverse order of priority
 
-# fish_add_path "$HOME"/.fly/bin
+if test -d "$HOME"/.fly/bin
+    fish_add_path "$HOME"/.fly/bin
+end
 
-# fish_add_path "$HOME"/.foundry/bin
-# fish_add_path "$HOME"/.cargo/bin
+if test -d "$HOME"/.cargo/bin
+    fish_add_path "$HOME"/.cargo/bin
+end
 
-fish_add_path "$HOME"/.local/share/pnpm
-set -U PNPM_HOME "$HOME"/.local/share/pnpm
+if test -d "$HOME"/.mix/escripts
+    fish_add_path "$HOME"/.mix/escripts
+end
 
-fish_add_path "$HOME"/.bun/bin
-set -U BUN_INSTALL "$HOME"/.bun
-set -Ux DO_NOT_TRACK 1
+if test -d "$HOME"/.foundry/bin
+    fish_add_path "$HOME"/.foundry/bin
+end
+
+if test -d "$HOME/.local/share/pnpm"
+    fish_add_path "$HOME"/.local/share/pnpm
+    set -U PNPM_HOME "$HOME"/.local/share/pnpm
+end
+
+if test -d "$HOME"/.bun/bin
+    fish_add_path "$HOME"/.bun/bin
+    set -U BUN_INSTALL "$HOME"/.bun
+    set -U BUN_CONFIG_NO_CLEAR_TERMINAL_ON_RELOAD 1
+    set -Ux BUN_CRASH_REPORTER_URL ''
+end
 
 fish_add_path "$HOME"/.local/bin
+
+set -Ux DO_NOT_TRACK 1
