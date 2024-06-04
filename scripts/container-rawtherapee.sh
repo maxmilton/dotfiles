4b1a56260232131c7c7e8a05f4ff8c5e4c707453
Max Milton <max@happyhackers.xyz>
2024-06-05 08:47:30 +0900

    feat: Add rawtherapee bootstrap script

---
+++ scripts/container-rawtherapee.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -eu
+
+# TODO: Populate persistent data rather than populate on every run
+doas pacman -S --noconfirm --needed archlinux-keyring
+doas pacman-key --init
+doas pacman-key --populate archlinux
+
+doas pacman -S --noconfirm --needed rawtherapee
+
+export PULSE_SERVER=unix:/run/user/host/pulse/native
+export DISPLAY=:0
+export WAYLAND_DISPLAY=/run/user/host/wayland-0
+export XDG_SESSION_TYPE=wayland
+export QT_QPA_PLATFORM=wayland
+export MOZ_ENABLE_WAYLAND=1
+export GTK_THEME=Adwaita:dark
+/usr/bin/rawtherapee $@
