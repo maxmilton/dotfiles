351adbd02a8017a297c2879b6cd21fcda9a30991
Max Milton <max@happyhackers.xyz>
2024-12-16 22:58:26 +0900

    feat: Tweaks

--- .config/fish/functions/clr.fish
+++ .config/fish/functions/clr.fish
@@ -1,6 +1,7 @@
 function clr
-  printf "\x1Bc"
+    printf "\x1Bc"
 
-  # clear scrollback buffer on macOS
-  printf '\x1B[3J'
+    # clear scrollback buffer on macOS
+    # printf '\x1B[3J'
+    printf '\x1B[2J\x1B[3J\x1B[H'
 end
--- .config/fish/functions/pp.fish
+++ .config/fish/functions/pp.fish
@@ -1,38 +1,38 @@
 function pp
-  set -l normal (set_color normal)
-  set -l yellow (set_color --bold bryellow)
+    set -l normal (set_color normal)
+    set -l yellow (set_color --bold bryellow)
 
-  echo -e "$yellow""Updating system...""$normal"
-  paru -Syu
+    echo -e "$yellow""Updating system...""$normal"
+    paru -Syu
 
-  echo -e "$yellow""Updating brave container""$normal"
-  paru -Syur /home/max/.machines/brave
+    echo -e "$yellow""Updating brave container""$normal"
+    paru -Syur /home/max/.machines/brave
 
-  # echo -e "$yellow""Updating code container""$normal"
-  # paru -Syur /home/max/.machines/code
+    # echo -e "$yellow""Updating code container""$normal"
+    # paru -Syur /home/max/.machines/code
 
-  echo -e "$yellow""Updating librewolf container""$normal"
-  paru -Syur /home/max/.machines/librewolf
-  # paru -Sr /home/max/.machines/librewolf librewolf-bin --nodeps
+    echo -e "$yellow""Updating librewolf container""$normal"
+    paru -Syur /home/max/.machines/librewolf
+    # paru -Sr /home/max/.machines/librewolf librewolf-bin --nodeps
 
-  echo -e "$yellow""Updating vms container""$normal"
-  sudo pacman -Syur /home/max/.machines/vms
+    echo -e "$yellow""Updating vms container""$normal"
+    sudo pacman -Syur /home/max/.machines/vms
 
-  echo -e "$yellow""Updating arch container""$normal"
-  sudo pacman -Syur /var/lib/machines/arch
+    echo -e "$yellow""Updating arch container""$normal"
+    sudo pacman -Syur /var/lib/machines/arch
 
-  # echo -e "$yellow""Updating cachyos container""$normal"
-  # sudo systemd-nspawn --capability=CAP_IPC_LOCK -D /var/lib/machines/cachyos sh -c "pacman -Syu"
+    # echo -e "$yellow""Updating cachyos container""$normal"
+    # sudo systemd-nspawn --capability=CAP_IPC_LOCK -D /var/lib/machines/cachyos sh -c "pacman -Syu"
 
-  # echo -e "$yellow""Updating alpine container""$normal"
-  # sudo systemd-nspawn -D /var/lib/machines/alpine sh -c "apk upgrade"
+    # echo -e "$yellow""Updating alpine container""$normal"
+    # sudo systemd-nspawn -D /var/lib/machines/alpine sh -c "apk upgrade"
 
-  # echo -e "$yellow""Updating lutris container""$normal"
-  # sudo systemd-nspawn --capability=CAP_IPC_LOCK -D /var/lib/machines/lutris sh -c "pacman -Syu"
+    # echo -e "$yellow""Updating lutris container""$normal"
+    # sudo systemd-nspawn --capability=CAP_IPC_LOCK -D /var/lib/machines/lutris sh -c "pacman -Syu"
 
-  echo -e "$yellow""Updating steam container""$normal"
-  sudo systemd-nspawn --capability=CAP_IPC_LOCK -D /var/lib/machines/steam sh -c "paru -Syu"
+    echo -e "$yellow""Updating steam container""$normal"
+    sudo systemd-nspawn --capability=CAP_IPC_LOCK -D /var/lib/machines/steam sh -c "paru -Syu"
 
-  echo -e "$yellow""Updating system firmware...""$normal"
-  fwupdmgr refresh && sudo fwupdmgr update
+    echo -e "$yellow""Updating system firmware...""$normal"
+    fwupdmgr refresh && sudo fwupdmgr update
 end
