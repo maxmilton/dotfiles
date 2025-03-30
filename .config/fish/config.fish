7397235a43e0d22ad2f8f768645bbfff7a088ebc
Max Milton <max@happyhackers.xyz>
2025-03-30 11:17:04 +0900

    feat: Tweak fish abbrs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,9 +19,10 @@ abbr h helix
 abbr p paru
 abbr curl "curl -vv -H 'Cache-Control: no-cache'"
 abbr mm 'mpv --no-video'
+abbr duckdb 'duckdb -safe -readonly'
 
 abbr bi 'bun install'
-abbr br 'rm -rf bun.lock*; rm -rf **/node_modules; bun install -f'
+abbr br 'rm -f bun.lock*; rm -rf **/node_modules; bun install -f'
 abbr bu "bunx --bun taze -rflIw --ignore-paths='**/*.bak/**' latest"
 abbr bb 'bun run build'
 abbr bd 'bun run dev'
@@ -32,6 +33,7 @@ abbr yi 'bunx yarn info'
 abbr gs 'git status --short --branch --show-stash'
 abbr gd 'git diff'
 abbr gdd 'git diff --staged'
+abbr gb 'git branch -vv --all'
 abbr gp 'git pull --prune --autostash'
 abbr gm 'git merge --autostash'
 abbr gc 'git commit -v'
