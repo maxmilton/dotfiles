0400e07d642acb38a791e8d27a63bd36f9180d6a
Max Milton <max@happyhackers.xyz>
2025-03-30 11:16:52 +0900

    feat: Improve git gg fish script

--- .config/fish/functions/gg.fish
+++ .config/fish/functions/gg.fish
@@ -1,3 +1,8 @@
 function gg
-    git checkout (git branch --all --format "%(refname:lstrip=2)" | fzy --lines max --query=$argv[1])
+    set -l branch (git branch --all --format "%(refname:lstrip=2)" | fzy --lines max --query="$argv")
+    if test -n "$branch"
+        git switch "$branch"
+    else
+        return 1
+    end
 end
