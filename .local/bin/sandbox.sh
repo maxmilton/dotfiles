9e830a57617259cf10d735fa6946520a77197b08
Max Milton <max@happyhackers.xyz>
2025-05-05 17:14:13 +0900

    feat: Rework VS Code config + add sandboxed shell

---
+++ .local/bin/sandbox.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/busybox sh
+set -euo pipefail
+
+exec 11<<EOF
+$(getent passwd "$(id -u)" 65534)
+EOF
+
+exec 12<<EOF
+$(getent group "$(id -g)" 65534)
+EOF
+
+exec bwrap \
+  --ro-bind /usr /usr \
+  --symlink usr/lib64 /lib64 \
+  --proc /proc \
+  --dev /dev \
+  --ro-bind /etc/ca-certificates /etc/ca-certificates \
+  --ro-bind /etc/ssl /etc/ssl \
+  --ro-bind /etc/resolv.conf /etc/resolv.conf \
+  --ro-bind "$HOME/Projects/dotfiles/.profile" "$HOME/.profile" \
+  --bind "$HOME/Downloads" "$HOME/Downloads" \
+  --bind "$HOME/Projects" "$HOME/Projects" \
+  --unshare-all \
+  --share-net \
+  --die-with-parent \
+  --new-session \
+  --clearenv \
+  --setenv HOME "$HOME" \
+  --setenv LC_ALL C.UTF-8 \
+  --setenv TERM "${TERM:-linux}" \
+  --setenv XDG_RUNTIME_DIR "${XDG_RUNTIME_DIR:-/tmp}" \
+  --setenv XDG_SESSION_TYPE "${XDG_SESSION_TYPE:-tty}" \
+  --file 11 /etc/passwd \
+  --file 12 /etc/group \
+  /usr/bin/busybox sh --login
+
+# TODO: To run GUI apps: https://sloonz.github.io/posts/sandboxing-2/
