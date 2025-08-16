49ecd30703204211ada426c16946f29d76a09f80
Max Milton <max@2okki.com>
2025-08-17 03:05:18 +0900

    feat: Refactor git config and settings

--- .config/fish/functions/gg.fish
+++ .config/fish/functions/gg.fish
@@ -1,5 +1,5 @@
 function gg
-    set -l branch (git branch --all --format "%(refname:lstrip=2)" | fzy --lines max --query="$argv")
+    set -l branch (git branch --sort=-committerdate --format "%(refname:short)" | fzy --lines max --query="$argv")
     if test -n "$branch"
         git switch "$branch"
     else
