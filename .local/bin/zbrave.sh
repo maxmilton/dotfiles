8c437d0244648c33b4e5545ef683d03c96134d54
Max Milton <max@2okki.com>
2025-11-03 05:12:47 +0900

    chore: Tweak scripts

--- .local/bin/zbrave.sh
+++ .local/bin/zbrave.sh
@@ -5,7 +5,7 @@ pkexec systemd-nspawn \
   --user=xxxx --chdir=/home/xxxx \
   --bind-ro="${XDG_RUNTIME_DIR}/pulse/native":/run/user/host/pulse/native \
   --bind-ro="${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY:-wayland-0}":/run/user/host/wayland-0 \
-  --bind=/dev/dri/card0 \
+  --bind=/dev/dri/card1 \
   --bind=/dev/dri/renderD128 \
   --bind=/tmp/.X11-unix/X0 \
   --bind-ro=/usr/share/fonts \
